802d465f97bcb8b3fcf00d48be3d19b566b3e3e8
jason <>
2024-11-30 22:26:38 -0500

    update configs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,8 +8,13 @@ return {
 
 	config = function()
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local lsp = require("lspconfig")
-
-		lsp.lua_ls.setup({ capabilities = capabilities })
+		ensure_installed = { "lua_ls" }
+		handlers = {
+			function(server_name)
+				require("lspconfig")[server_name].setup({
+					capabilities = capabilities,
+				})
+			end,
+		}
 	end,
 }
